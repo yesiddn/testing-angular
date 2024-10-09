7705d03bb9cd6ae6d4bb34589032a037b605f6c2
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-09 11:30:54 -0500

    Add Products component with grid layout and product fetching logic

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.mode';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  private productService = inject(ProductService);
+  products: Product[] = [];
 
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        console.log('Products:', products);
+        this.products = products;
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    })
+  }
 }
