e6be647d7efec05dc2d608d8d8ba53829e5cb9d0
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-24 21:14:23 -0500

    Add unit tests for ProductsComponent with ProductService mock

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,23 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import { ProductComponent } from '../product/product.component';
+import { ProductService } from '../../services/product.service';
+import { generateManyProducts } from '../../models/product.mock';
+import { of } from 'rxjs';
 
-describe('ProductsComponent', () => {
+fdescribe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: jasmine.SpyObj<ProductService>
 
   beforeEach(async () => {
+    const productServiceSpy = jasmine.createSpyObj('ProductService', ['getAllProducts']);
+
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent]
+      imports: [ProductsComponent, ProductComponent],
+      providers: [
+        {
+          provide: ProductService,
+          useValue: productServiceSpy
+        }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    productService = TestBed.inject(ProductService) as jasmine.SpyObj<ProductService>; // se obtiene el servicio como un spy
   });
 
   it('should create', () => {
+    const productsMock = generateManyProducts(3);
+    productService.getAllProducts.and.returnValue(of(productsMock)); // se configura el spy para que retorne un valor específico
+
+    fixture.detectChanges(); // se ejecuta el ngOnInit
+
     expect(component).toBeTruthy();
+    expect(productService.getAllProducts).toHaveBeenCalled();
   });
 });
