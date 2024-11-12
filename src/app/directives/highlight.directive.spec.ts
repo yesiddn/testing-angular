d7c45c1581b6a771d8c7d4a0a88930fbf7576911
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-11 23:15:45 -0500

    Add tests to directives

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,61 @@
+import { Component } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  standalone: true,
+  imports: [HighlightDirective],
+  template: `
+    <h5 class="title" highlight>default color</h5>
+    <h5 highlight="yellow">yellow</h5>
+    <p highlight="blue">default paragraph color</p>
+    <p>default paragraph without highlight</p>
+  `,
+})
+class HostComponent {
+}
 
 describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HostComponent, HighlightDirective],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have three highlighted elements', () => {
+    //const elements = fixture.debugElement.queryAll(By.css('*[highlight]')); // * means any element
+    const elements = fixture.debugElement.queryAll(By.directive(HighlightDirective)); // this is better
+    const withoutHighlight = fixture.debugElement.queryAll(By.css('*:not([highlight])'));
+
+    expect(elements.length).toBe(3);
+    expect(withoutHighlight.length).toBe(1);
+  });
+
+  it('should the elements be match with bgColor', () => {
+    const elements = fixture.debugElement.queryAll(By.directive(HighlightDirective));
+    expect(elements[0].nativeElement.style.backgroundColor).toBe('gray');
+    expect(elements[1].nativeElement.style.backgroundColor).toBe('yellow');
+    expect(elements[2].nativeElement.style.backgroundColor).toBe('blue');
+
+    expect(elements.length).toBe(3);
+  });
+
+  it('should the h5.title be defaultColor', () => {
+    const titleDe = fixture.debugElement.query(By.css('.title'));
+    const directive = titleDe.injector.get(HighlightDirective); // get the directive instance
+
+    expect(titleDe.nativeElement.style.backgroundColor).toBe(directive.defaultColor); // gray
   });
 });
